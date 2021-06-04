8ec1945ff5c9608fc43a6d5ce65372c674380444
Michael Godeck <mgodeck@gmail.com>
2021-06-03 23:58:58 -0500

    netlify adaptor
    Signed-off-by: Michael Godeck <mgodeck@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@sveltejs/kit').Config} */
-const netlify = require('@sveltejs/adapter-netlify')
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
